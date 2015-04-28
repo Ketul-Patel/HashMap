581a51bdaf595ac6b4f6e121ed7319f236dcd535
Ketul Patel <kjpatel2010@gmail.com>
2015-04-27 19:03:26 -0700

    Initial Commit

---
+++ HashMap/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HashMap
+//
+//  Created by Ketul Patel on 4/27/15.
+//  Copyright (c) 2015 Ketul Patel. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
